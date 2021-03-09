003ed2a431a9ab6f4487fab5a4d6c71109a25e5f
Akash <as55848@gmail.com>
2021-03-09 09:29:16 +0530

    add new mvc demo project with name demo1

---
+++ demo1/demo1/Models/myIndexModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demo1.Models
+{
+    public class myIndexModel
+    {
+        
+        public int Id { get; set; }
+        public string EmailId { get; set; }
+        public int Pwd { get; set; }
+    }
+}
